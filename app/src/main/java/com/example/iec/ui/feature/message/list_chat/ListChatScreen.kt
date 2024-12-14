ccbbd645a4b8cb4dcb22e43d19da33366afc2a42
anhnt1061 <anhnt1061@ghtk.co>
2024-12-14 16:29:30 +0700

    Navigation

--- app/src/main/java/com/example/iec/ui/feature/message/list_chat/ListChatScreen.kt
+++ app/src/main/java/com/example/iec/ui/feature/message/list_chat/ListChatScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 
 
 sealed class ListChatState{
+    data class Error(val message: String): ListChatState()
     data object Loading : ListChatState()
     data class AppState(val listUserOnline: List<String>) : ListChatState()
 }
